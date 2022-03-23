4d451fcbd029ff9eeaea64b8cb8d27f0c1e236e1
damirdine <damiridne@outlook.fr>
2022-03-23 15:32:50 +0100

    stockage stats avec localStorage

---
+++ README.md
@@ -0,0 +1,18 @@
+# Un chifumi en JavaScript.
+
+Dans cet exercice j'ai eu à utiliser : 
+* les conditions Javascript notamment avec les opérateurs `&` et `||`.
+* les objets tableau Javascript.
+* La propriété `localStorage` pour stocker des valeurs et les réutiliser.
+
+
+Les conditions algorithmiques auquel j'ai pensé :
+- si  (choix=choixAleatoire)
+   alors on à une égalité
+----
+- si ( ***p - c***  OU  ***c - f***  OU  ***f - p*** ) 
+     alors le joueur gagne
+-----
+- si (***p - f*** OU ***c - p*** OU ***f - c***)
+     alors le joueur perd
+-----
--- src/app.js
+++ src/app.js
@@ -9,31 +9,51 @@ let nulCountTxt = document.querySelector('#manche-nul');
 let loseCountTxt = document.querySelector('#manche-perdu');
 let saveBtn = document.querySelector('#save');
 let resetBtn = document.querySelector('#reset');
-
-
 let choixJoueur;
 let choixRandom;
 let resultat;
 let saves;
-
 let winCount =0;
 let nulCount =0;
 let loseCount =0;
-//console.log(`Random IS NUMBER ${RandomChoix}`);
+
+function verifSave(){
+    if(!localStorage.length=='')
+    {
+        winCount=localStorage.getItem('winCountSave')
+        nulCount=localStorage.getItem('nulCountSave')
+        loseCount=localStorage.getItem('loseCountSave')
+        stats('localeStorageEmpty')
+    }else{
+        stats('reset')
+    }
+
+    return [winCount,nulCount,loseCount]
+}
+verifSave()
 
 function stats (resultat){
-
     if (resultat==='Manche Gagné'){
         winCount ++;
-        winCountTxt.innerHTML= `Manche gagné = ${winCount}`
+        winCountTxt.innerHTML= `Manche gagnés = ${winCount}`
     }
     if (resultat==='Manche Nul'){
         nulCount ++;
-        nulCountTxt.innerHTML= `Manche Nul = ${nulCount}`
+        nulCountTxt.innerHTML= `Manche nul = ${nulCount}`
     }
     if (resultat==='Manche Perdu'){
         loseCount ++;
-        loseCountTxt.innerHTML= `Manche Perdu = ${loseCount}`
+        loseCountTxt.innerHTML= `Manche perdu = ${loseCount}`
+    }
+    if(resultat==="localeStorageEmpty"){
+        winCountTxt.innerHTML= `Manche gagnés = ${winCount}`
+        nulCountTxt.innerHTML= `Manche nul = ${nulCount}`
+        loseCountTxt.innerHTML= `Manche perdu = ${loseCount}`   
+    }
+    if(resultat==='reset'){
+        winCountTxt.innerHTML= `Manche gagnés = 0`;
+        nulCountTxt.innerHTML= `Manche nuls = 0`;
+        loseCountTxt.innerHTML= `Manche perdus = 0`;
     }
 }
 
@@ -72,7 +92,30 @@ function jeux(){
                 resultatText.innerHTML =`resultat : ${resultat}`
             },1000
         ),
-        stats(resultat)
+        stats(resultat),
+        save()
     ]
 };
+
 btn.addEventListener('click', jeux);
+
+function save() {
+    saveBtn.addEventListener('click',function (){
+        localStorage.setItem('winCountSave',winCount);
+        localStorage.setItem('nulCountSave',nulCount);
+        localStorage.setItem('loseCountSave',loseCount);
+
+        winCount = localStorage.getItem('winCountSave');
+        nulCount = localStorage.getItem('nulCountSave');
+        loseCount = localStorage.getItem('loseCountSave');
+    });
+    resetBtn.addEventListener('click',function(){
+        localStorage.clear();
+        winCount =  0;
+        nulCount = 0;
+        loseCount = 0;
+        stats('reset')    
+    });
+    return [winCount,nulCount,loseCount]
+}
+save();
