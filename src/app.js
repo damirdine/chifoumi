8eb56b7c76890ea7f801c2b2816ee2a1040845e0
damirdine <damiridne@outlook.fr>
2022-03-23 11:15:45 +0100

    resultat de la manche avec les statistiques

--- src/app.js
+++ src/app.js
@@ -1,3 +1,78 @@
-const radio = document.querySelectorAll('input[type:"radio"]');
+const choix = ['pierre' , 'feuille', 'ciseaux'];
 
-console.log(radio)
+let radios = document.querySelectorAll('input[type="radio"]');
+let btn = document.querySelector('button#play');
+let resultatText = document.querySelector('#resultat');
+let choixJoueursText = document.querySelector('#choixJoueurs');
+let winCountTxt = document.querySelector('#manche-gagner');
+let nulCountTxt = document.querySelector('#manche-nul');
+let loseCountTxt = document.querySelector('#manche-perdu');
+let saveBtn = document.querySelector('#save');
+let resetBtn = document.querySelector('#reset');
+
+
+let choixJoueur;
+let choixRandom;
+let resultat;
+let saves;
+
+let winCount =0;
+let nulCount =0;
+let loseCount =0;
+//console.log(`Random IS NUMBER ${RandomChoix}`);
+
+function stats (resultat){
+
+    if (resultat==='Manche Gagné'){
+        winCount ++;
+        winCountTxt.innerHTML= `Manche gagné = ${winCount}`
+    }
+    if (resultat==='Manche Nul'){
+        nulCount ++;
+        nulCountTxt.innerHTML= `Manche Nul = ${nulCount}`
+    }
+    if (resultat==='Manche Perdu'){
+        loseCount ++;
+        loseCountTxt.innerHTML= `Manche Perdu = ${loseCount}`
+    }
+}
+
+function jeux(){
+    choixRandom = Math.floor(Math.random() * choix.length);
+    for(i = 0; i < radios.length; i++){
+        if(radios[i].checked){
+            choixJoueur = radios[i].value;
+            choixRandom = radios[choixRandom].value;
+          if(
+             (choixJoueur==='pierre' & choixRandom==='ciseaux') ||
+             (choixJoueur==='ciseaux' & choixRandom==='feuille') ||
+             (choixJoueur==='feuille' & choixRandom==='pierre')
+          )
+          {
+            resultat = 'Manche Gagné';   
+          }
+          if(choixJoueur===choixRandom)
+          {
+            resultat = 'Manche Nul'
+          }if(
+             (choixJoueur==='pierre' & choixRandom==='feuille') ||
+             (choixJoueur==='ciseaux' & choixRandom==='pierre') ||
+             (choixJoueur==='feuille' & choixRandom==='ciseaux')
+          ){
+            resultat = 'Manche Perdu'
+          }
+
+        }
+    }
+    return [
+        resultatText.innerHTML = 'resultat : ',
+        choixJoueursText.innerHTML=`Tu a joué ${choixJoueur} et moi ${choixRandom}`,
+        setTimeout(
+            function(){
+                resultatText.innerHTML =`resultat : ${resultat}`
+            },1000
+        ),
+        stats(resultat)
+    ]
+};
+btn.addEventListener('click', jeux);
