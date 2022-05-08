cfb935c4c3ea2a5d32c056c204ab0cad354dfc4f
damirdine <damirdine@outlook.fr>
2022-05-08 21:33:56 +0200

    save to localstorage

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const choix = ['pierre' , 'feuille', 'ciseaux'];
 let choixJoueur,choixOrdinateur,resultatManche
-let btns = document.querySelectorAll('input')
-//console.log(btns)
+let btns = document.querySelectorAll('.choix-joueur')
+
 function choixRandom(choixPossible){
   let NbrRandom = Math.floor(Math.random() * choixPossible.length)
   return choixOrdinateur = choixPossible[NbrRandom]
@@ -15,12 +15,10 @@ function manche(){
       (choixJoueur==='feuille' & choixOrdinateur==='pierre')
     ){
     resultatManche = 'Manche Gagné';
-  }
-    
+  } 
   if(choixJoueur===choixOrdinateur){
     resultatManche = 'Manche Nul'
-  }
-    
+  }  
   if(
       (choixJoueur==='pierre' & choixOrdinateur==='feuille') ||
       (choixJoueur==='ciseaux' & choixOrdinateur==='pierre') ||
@@ -30,23 +28,51 @@ function manche(){
   }
   return resultatManche
 }
+
 function afficherResultat(param){
   let resultatMancheTxt = document.querySelector('#resultat-manche')
   let jeuFait = document.querySelector('#jeu')
   jeuFait.textContent = `${choixJoueur} contre ${choixOrdinateur}`
   resultatMancheTxt.textContent = resultatManche
 }
-let mancheGagne=0,mancheNul=0,manchePerdu=0
+
+let mancheGagne,mancheNul,manchePerdu
+
 function compteur(resultatManche){
-  if(resultatManche==='Manche Gagné')
-  {
-     mancheGagne +=1
-  }
-  if(resultatManche==='Manche Nul'){
-     mancheNul+=1
-  }
-  if(resultatManche==='Manche Perdu'){
-     manchePerdu+=1
+  if(localStorage.length==''){
+    mancheGagne=0
+    mancheNul=0
+    manchePerdu=0
+    if(resultatManche==='Manche Gagné')
+    {
+      mancheGagne +=1
+      localStorage.setItem('winCount',mancheGagne);
+    }
+    if(resultatManche==='Manche Nul'){
+      mancheNul+=1
+      localStorage.setItem('nulCount',mancheNul);
+    }
+    if(resultatManche==='Manche Perdu'){
+      manchePerdu+=1
+      localStorage.setItem('loseCount',manchePerdu);
+    }
+  }else{
+    mancheGagne=Number(localStorage.getItem('winCount'))
+    mancheNul=Number(localStorage.getItem('nulCount'))
+    manchePerdu=Number(localStorage.getItem('loseCount'))
+    if(resultatManche==='Manche Gagné')
+    {
+      mancheGagne +=1
+      localStorage.setItem('winCount',mancheGagne);
+    }
+    if(resultatManche==='Manche Nul'){
+      mancheNul+=1
+      localStorage.setItem('nulCount',mancheNul);
+    }
+    if(resultatManche==='Manche Perdu'){
+      manchePerdu+=1
+      localStorage.setItem('loseCount',manchePerdu);
+    }
   }
   return [mancheGagne,mancheNul,manchePerdu]
 }
@@ -55,10 +81,12 @@ function afficherStat(stats){
   let statsTxt = document.querySelector('#stats')
   statsTxt.textContent = `${mancheGagne} - ${mancheNul} - ${manchePerdu}`
 }
+
 function jeu(){
   afficherResultat(manche())
   afficherStat(compteur(resultatManche))
 }
+
 btns.forEach(element => 
   element.addEventListener('click', event => {
     choixJoueur=element.value
