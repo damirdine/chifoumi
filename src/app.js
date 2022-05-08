b281f9ae9ee10189723273731ee31e079d3cc7a9
damirdine <damirdine@outlook.fr>
2022-05-08 20:18:02 +0200

    new js base

--- src/app.js
+++ src/app.js
@@ -1,121 +1,67 @@
 const choix = ['pierre' , 'feuille', 'ciseaux'];
-
-let radios = document.querySelectorAll('input[type="radio"]');
-let btn = document.querySelector('button#play');
-let resultatText = document.querySelector('#resultat');
-let choixJoueursText = document.querySelector('#choixJoueurs');
-let winCountTxt = document.querySelector('#manche-gagner');
-let nulCountTxt = document.querySelector('#manche-nul');
-let loseCountTxt = document.querySelector('#manche-perdu');
-let saveBtn = document.querySelector('#save');
-let resetBtn = document.querySelector('#reset');
-let choixJoueur;
-let choixRandom;
-let resultat;
-let saves;
-let winCount =0;
-let nulCount =0;
-let loseCount =0;
-
-function verifSave(){
-    if(!localStorage.length=='')
-    {
-        winCount=localStorage.getItem('winCountSave')
-        nulCount=localStorage.getItem('nulCountSave')
-        loseCount=localStorage.getItem('loseCountSave')
-        stats('localeStorageEmpty')
-    }else{
-        stats('reset')
-    }
-
-    return [winCount,nulCount,loseCount]
-}
-verifSave()
-
-function stats (resultat){
-    if (resultat==='Manche Gagné'){
-        winCount ++;
-        winCountTxt.innerHTML= `Manche gagnés = ${winCount}`
-    }
-    if (resultat==='Manche Nul'){
-        nulCount ++;
-        nulCountTxt.innerHTML= `Manche nul = ${nulCount}`
-    }
-    if (resultat==='Manche Perdu'){
-        loseCount ++;
-        loseCountTxt.innerHTML= `Manche perdu = ${loseCount}`
-    }
-    if(resultat==="localeStorageEmpty"){
-        winCountTxt.innerHTML= `Manche gagnés = ${winCount}`
-        nulCountTxt.innerHTML= `Manche nul = ${nulCount}`
-        loseCountTxt.innerHTML= `Manche perdu = ${loseCount}`   
-    }
-    if(resultat==='reset'){
-        winCountTxt.innerHTML= `Manche gagnés = 0`;
-        nulCountTxt.innerHTML= `Manche nuls = 0`;
-        loseCountTxt.innerHTML= `Manche perdus = 0`;
-    }
+let choixJoueur,choixOrdinateur,resultatManche
+let btns = document.querySelectorAll('input')
+//console.log(btns)
+function choixRandom(choixPossible){
+  let NbrRandom = Math.floor(Math.random() * choixPossible.length)
+  return choixOrdinateur = choixPossible[NbrRandom]
 }
 
-function jeux(){
-    choixRandom = Math.floor(Math.random() * choix.length);
-    for(i = 0; i < radios.length; i++){
-        if(radios[i].checked){
-            choixJoueur = radios[i].value;
-            choixRandom = radios[choixRandom].value;
-          if(
-             (choixJoueur==='pierre' & choixRandom==='ciseaux') ||
-             (choixJoueur==='ciseaux' & choixRandom==='feuille') ||
-             (choixJoueur==='feuille' & choixRandom==='pierre')
-          )
-          {
-            resultat = 'Manche Gagné';   
-          }
-          if(choixJoueur===choixRandom)
-          {
-            resultat = 'Manche Nul'
-          }if(
-             (choixJoueur==='pierre' & choixRandom==='feuille') ||
-             (choixJoueur==='ciseaux' & choixRandom==='pierre') ||
-             (choixJoueur==='feuille' & choixRandom==='ciseaux')
-          ){
-            resultat = 'Manche Perdu'
-          }
-
-        }
-    }
-    return [
-        resultatText.innerHTML = 'resultat : ',
-        choixJoueursText.innerHTML=`Tu a joué ${choixJoueur} et moi ${choixRandom}`,
-        setTimeout(
-            function(){
-                resultatText.innerHTML =`resultat : ${resultat}`
-            },1000
-        ),
-        stats(resultat),
-        save()
-    ]
-};
-
-btn.addEventListener('click', jeux);
-
-function save() {
-    saveBtn.addEventListener('click',function (){
-        localStorage.setItem('winCountSave',winCount);
-        localStorage.setItem('nulCountSave',nulCount);
-        localStorage.setItem('loseCountSave',loseCount);
-
-        winCount = localStorage.getItem('winCountSave');
-        nulCount = localStorage.getItem('nulCountSave');
-        loseCount = localStorage.getItem('loseCountSave');
-    });
-    resetBtn.addEventListener('click',function(){
-        localStorage.clear();
-        winCount =  0;
-        nulCount = 0;
-        loseCount = 0;
-        stats('reset')    
-    });
-    return [winCount,nulCount,loseCount]
+function manche(){
+  choixOrdinateur = choixRandom(choix)
+  if(
+      (choixJoueur==='pierre' & choixOrdinateur==='ciseaux') ||
+      (choixJoueur==='ciseaux' & choixOrdinateur==='feuille') ||
+      (choixJoueur==='feuille' & choixOrdinateur==='pierre')
+    ){
+    resultatManche = 'Manche Gagné';
+  }
+    
+  if(choixJoueur===choixOrdinateur){
+    resultatManche = 'Manche Nul'
+  }
+    
+  if(
+      (choixJoueur==='pierre' & choixOrdinateur==='feuille') ||
+      (choixJoueur==='ciseaux' & choixOrdinateur==='pierre') ||
+      (choixJoueur==='feuille' & choixOrdinateur==='ciseaux')
+    ){
+      resultatManche = 'Manche Perdu'
+  }
+  return resultatManche
 }
-save();
+function afficherResultat(param){
+  let resultatMancheTxt = document.querySelector('#resultat-manche')
+  let jeuFait = document.querySelector('#jeu')
+  jeuFait.textContent = `${choixJoueur} contre ${choixOrdinateur}`
+  resultatMancheTxt.textContent = resultatManche
+}
+let mancheGagne=0,mancheNul=0,manchePerdu=0
+function compteur(resultatManche){
+  if(resultatManche==='Manche Gagné')
+  {
+     mancheGagne +=1
+  }
+  if(resultatManche==='Manche Nul'){
+     mancheNul+=1
+  }
+  if(resultatManche==='Manche Perdu'){
+     manchePerdu+=1
+  }
+  return [mancheGagne,mancheNul,manchePerdu]
+}
+
+function afficherStat(stats){
+  let statsTxt = document.querySelector('#stats')
+  statsTxt.textContent = `${mancheGagne} - ${mancheNul} - ${manchePerdu}`
+}
+function jeu(){
+  afficherResultat(manche())
+  afficherStat(compteur(resultatManche))
+}
+btns.forEach(element => 
+  element.addEventListener('click', event => {
+    choixJoueur=element.value
+    jeu()
+  })
+)
